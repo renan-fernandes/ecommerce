8fdcf0e15d3cd3830baf17ba8b628b26cc668d0b
renan-fernandes <renanramos88@hotmail.com>
2018-09-19 20:59:01 -0300

    recuperar senha

--- index.php
+++ index.php
@@ -49,6 +49,144 @@ $app->get('/admin/logout', function() {
 	exit;	
 });
 
+$app->get('/admin/users', function() {
+	
+	User::verifyLogin();
+	
+	$users = User::listAll();
+	
+	$page = new PageAdmin(); //aqui queremos o header e o footer.
+	$page->setTpl("users", array(
+		"users" => $users
+	));
+});
+
+$app->get('/admin/users/create', function() {
+	
+	User::verifyLogin();
+	
+	$page = new PageAdmin(); 
+	$page->setTpl("users-create");
+});
+
+$app->get('/admin/users/:iduser/delete', function ($iduser) { //para excluir. //tem que ficar antes do $app->get('/admin/users/:iduser', para funcionar. senão, o slim pode achar que o /delete faz parte do :iduser caso a função $app->get('/admin/users/:iduser', seja lida primeiro. 
+	User::verifyLogin();
+	
+	$user = new User();
+	
+	$user->get((int)$iduser);
+	
+	$user->delete();
+	
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get('/admin/users/:iduser', function($iduser) {
+	User::verifyLogin();
+	$user = new User();
+ 
+    $user->get((int)$iduser);
+ 
+	$page = new PageAdmin();
+	$page->setTpl("users-update", array(
+		"user" => $user->getValues()
+	));
+});
+
+$app->post('/admin/users/create', function () { //vai receber em post e enviar para o banco de dados. 
+	User::verifyLogin();
+	
+	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+	
+	$user->setData($_POST); //vai criar o objeto a partir do array $_POST. cada índice do array tem o mesmo nome de atributo da classe, que é criado dinâmicamente. 
+	
+	$user->save();
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->post('/admin/users/:iduser', function ($iduser) { //para salvar a edição.
+	User::verifyLogin();
+
+	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+	
+	$user->get((int)$iduser);
+	
+	$user->setData($_POST);
+	
+	$user->update();
+	
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->get("/admin/forgot", function(){
+	$page = new PageAdmin([
+		"header" => false, //porque nessa página não precisamos do header padrão
+		"footer" => false //porque nessa página não precisamos do footer padrão
+	]);
+	$page->setTpl("forgot");
+	
+});
+
+$app->post("/admin/forgot", function(){
+	$user = User::getForgot($_POST["email"]);
+	
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+$app->get("/admin/forgot/sent", function() {
+	$page = new PageAdmin([
+		"header" => false, //porque nessa página não precisamos do header padrão
+		"footer" => false //porque nessa página não precisamos do footer padrão
+	]);
+	$page->setTpl("forgot-sent");
+	
+});
+
+$app->get("/admin/forgot/reset", function() {
+	$user = User::validForgotDecrypt($_GET["code"]);
+	
+	$page = new PageAdmin([
+		"header" => false, //porque nessa página não precisamos do header padrão
+		"footer" => false //porque nessa página não precisamos do footer padrão
+	]);
+	
+	$page->setTpl("forgot-reset", array(
+		"name" => $user["desperson"],
+		"code" => $_GET["code"]
+	));
+});
+
+$app->post("/admin/forgot/reset", function() {
+	$forgot = User::validForgotDecrypt($_POST["code"]); //verifica de novo para aumentar a segurança. a diferença é que agora é $_POST.
+	User::setForgotUsed($forgot["idrecovery"]);
+	
+	$user = new User();
+	$user->get((int)$forgot["iduser"]);
+	
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, array(
+		"cost" => 12
+	));
+	
+	$user->setPassword($password);
+	
+		$page = new PageAdmin([
+		"header" => false, //porque nessa página não precisamos do header padrão
+		"footer" => false //porque nessa página não precisamos do footer padrão
+	]);
+	
+	$page->setTpl("forgot-reset-success");
+	
+});
+
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -3,10 +3,12 @@
 namespace Hcode\Model;
 use \Hcode\DB\Sql; //o '\' antes de Hcode significa buscar a partir da raiz. 
 use \Hcode\Model;
+use \Hcode\Mailer; 
 
 class User extends Model
 {
 	const SESSION = "User"; //definindo a constante SESSION.
+	const SECRET = "HcodePhp7_Secret"; //essa chave deve ter no mín, 16 caracteres. pode ter mais, mas são valores fixos, tipo: 16, 24, 32, etc. NÃO SUBIR A CHAVE EM REPOSITÓRIOS PÚBLICOS!
 	
 	public static function login($login, $password)
 	{
@@ -16,6 +18,7 @@ class User extends Model
 			":LOGIN" => $login //bind dos parâmetros		
 		));
 		
+		var_dump($login);
 		if(count($results) === 0) //se não encontrou nada, podemos lançar uma exceção. 
 		{
 			throw new \Exception("Usuário inexistente ou senha inválida."); //a exceção está no escopo/namespace principal do PHP e não dentro do namespace Hcode\Model, pois não criamos nossa própria exception, por isso precisamos colocar a '\' pra ele achar a Exception principal. 
@@ -56,6 +59,168 @@ class User extends Model
 	public static function logOut()
 	{
 		$_SESSION[User::SESSION] = null;
+	}
+	
+	public static function listAll()
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");	
+	}
+	
+	public function get($iduser)
+	{
+	 
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+		":iduser"=>$iduser
+		));
+
+		$data = $results[0];
+
+		$this->setData($data);
+	}
+	
+	public function update()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser" => $this->getiduser(),
+			":desperson" => $this->getdesperson(),
+			":deslogin" => $this->getdeslogin(),
+			":despassword" => $this->getdespassword(),
+			":desemail" => $this->getdesemail(),
+			":nrphone" => $this->getnrphone(),
+			":inadmin" => $this->getinadmin()
+		));
+		
+		$this->setData($results[0]);
+	}
+	
+	public function delete()
+	{
+		$sql = new Sql();
+		
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser" => $this->getiduser()
+		));
+		
+	}
+	
+	public function save()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson" => $this->getdesperson(),
+			":deslogin" => $this->getdeslogin(),
+			":despassword" => $this->getdespassword(),
+			":desemail" => $this->getdesemail(),
+			":nrphone" => $this->getnrphone(),
+			":inadmin" => $this->getinadmin()
+		));
+		
+		$this->setData($results[0]);
+	}
+	
+	public static function getForgot($email)
+	{
+		$sql = new Sql();
+		$results = $sql->select("
+			SELECT * 
+			FROM tb_persons a
+			INNER JOIN tb_users b USING(idperson)
+			WHERE a.desemail = :email;", array(
+				":email" => $email
+			));
+		
+		if(count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+			$data = $results[0];
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+				":iduser" => $data["iduser"],
+				":desip" => $_SERVER["REMOTE_ADDR"]
+			));
+			
+			if(count($results2) === 0)
+			{
+				throw new \Exception("Não foi possível recuperar a senha.");				
+			}
+			else
+			{
+				$dataRecovery = $results2[0];
+				
+				//$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+				$code = $dataRecovery["idrecovery"]; ///teste
+				
+				$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+				
+				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
+					"name" => $data["desperson"],
+					"link" => $link				
+				));
+				
+				$mailer->send();
+				
+				return $data;				
+			}
+		}
+	}
+	
+	public static function validForgotDecrypt($code)
+	{
+		//$idrecovery = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, User::SECRET, base64_decode($code), MCRYPT_MODE_ECB);
+		$idrecovery = $code;
+		
+		$sql = new Sql();
+		
+		$results = $sql->select("
+			SELECT *
+			FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();", array(
+					"idrecovery" => $idrecovery,			
+				));
+		
+		if(count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+			return $results[0];
+		}
+	}
+	
+	public static function setForgotUsed($idrecovery)
+	{
+		$sql = new Sql();
+		
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW WHERE idrecovery = :idrecovery", array(
+			":idrecovery" => $idrecovery		
+		));		
+	}
+	
+	public function setPassword($password)
+	{
+		$sql = new Sql();
+		
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password" => $password,
+			":iduser" => $this->getiduser()
+		));
+		
 	}
 }
 
